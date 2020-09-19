44252c08d452e75c92dc812c4bcfe3692e33f12c
Ivelina Daiss <ivelina.daiss.ubc@gmail.com>
2020-09-18 17:12:33 -0700

    in progress

--- src/main/java/cpen221practice/Classification.java
+++ src/main/java/cpen221practice/Classification.java
@@ -1,5 +1,5 @@
 package cpen221practice;
-
+//this is a class on it's own
 enum Classification {
 
     DEFICIENT, PERFECT, ABUNDANT
--- src/main/java/cpen221practice/NaturalNumber.java
+++ src/main/java/cpen221practice/NaturalNumber.java
@@ -1,7 +1,34 @@
 package cpen221practice;
 
 class NaturalNumber {
+    //check if the number i
+    // s the natural number and calculate the aliquot sum
+    private int num;//this is the input number
 
+    public NaturalNumber(int number) {
+        num = number;
+    }
 
+    public int getAquilotSum() {
 
+        int sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum = sum + num;
+            }
+        }
+    }
+    public enum getClassification() {
+        if(sum == num)
+        {
+            return Classification.PERFECT;
+        } else if(sum < num)
+        {
+            return Classification.DEFICIENT;
+        } else
+        {
+            return Classification.ABUNDANT;
+        }
+    }
 }
+
